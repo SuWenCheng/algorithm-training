cc47a3e82c2525d5ed0a5b79082bffc945633f39
suwencheng <wencheng@imagedt.com>
2020-09-27 11:43:42 +0800

    根据一棵树的中序遍历与后序遍历构造二叉树

--- src/main/java/algorithm/ThreeSumClosest.java
+++ src/main/java/algorithm/ThreeSumClosest.java
@@ -6,22 +6,22 @@ import org.junit.Test;
 import java.util.Arrays;
 
 /**
- * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * 给定一个包括n 个整数的数组nums和 一个目标值target。找出nums中的三个整数，使得它们的和与target最接近。返回这三个数的和。假定每组输入只存在唯一答案。
  *
- *  
+ * 
  *
  * 示例：
  *
  * 输入：nums = [-1,2,1,-4], target = 1
  * 输出：2
  * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
- *  
+ * 
  *
  * 提示：
  *
  * 3 <= nums.length <= 10^3
- * -10^3 <= nums[i] <= 10^3
- * -10^4 <= target <= 10^4
+ * -10^3<= nums[i]<= 10^3
+ * -10^4<= target<= 10^4
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/3sum-closest
