a07b855f522ff1bfbe3270ef0794655c8d7c525a
suwencheng <wencheng@imagedt.com>
2020-10-25 02:09:40 +0800

    视频拼接

--- src/main/java/algorithm/ThreeSumClosest.java
+++ src/main/java/algorithm/ThreeSumClosest.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 import java.util.Arrays;
 
 /**
- * 给定一个包括n 个整数的数组nums和 一个目标值target。找出nums中的三个整数，使得它们的和与target最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * 给定一个包括n 个整数的数组nums和 一个目标值target。
+ * 找出nums中的三个整数，使得它们的和与target最接近。返回这三个数的和。假定每组输入只存在唯一答案。
  *
  * 
  *
