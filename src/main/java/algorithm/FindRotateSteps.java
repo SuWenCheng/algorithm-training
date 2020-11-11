6de24b709ea2c80a7558a07df250eb2d8f4f6f74
suwencheng <wencheng@imagedt.com>
2020-11-12 01:00:58 +0800

    自由之路(动态规划解法)

--- src/main/java/algorithm/FindRotateSteps.java
+++ src/main/java/algorithm/FindRotateSteps.java
@@ -85,11 +85,43 @@ public class FindRotateSteps {
         return res;
     }
 
+    /**
+     * 动态递归
+     */
+    public int findRotateSteps2(String ring, String key) {
+        int n = ring.length(), m = key.length();
+        List<Integer>[] pos = new List[26];
+        for (int i = 0; i < 26; ++i) {
+            pos[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < n; ++i) {
+            pos[ring.charAt(i) - 'a'].add(i);
+        }
+        // dp[i][j]表示key前i个字符走过的最少步数（其中key[i]==ring[j]）
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            Arrays.fill(dp[i], 0x3f3f3f);
+        }
+        for (int i : pos[key.charAt(0) - 'a']) {
+            dp[0][i] = Math.min(i, n - i) + 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j : pos[key.charAt(i) - 'a']) {
+                for (int k : pos[key.charAt(i - 1) - 'a']) {
+                    log.info("i : {}  j : {}", i, j);
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][k] + Math.min(Math.abs(j - k), n - Math.abs(j - k)) + 1);
+                }
+            }
+        }
+        return Arrays.stream(dp[m - 1]).min().getAsInt();
+    }
+
     @Test
     public void test() {
         String ring = "godding";
         String key = "gd";
-        log.info(findRotateSteps(ring, key) + "");
+        log.info(findRotateSteps2(ring, key) + "");
+
     }
 
 }
